98473ea1296fe4029a6bd1c4d831fd710ef00b7d
nfadili <nfadili@uw.edu>
2016-04-03 12:41:59 -0700

    Implemented the newNote function for adding notes to a midi track in more intuitive way. Added parseDuration function for converting standard note durations into midi ticks.

--- src/Note.py
+++ src/Note.py
@@ -1,14 +1,21 @@
+import midi
+
+
 # Add self to param list if you end up making a class/API
 # Parse to -> data[NOTE, VELOCITY]
-def newNote(pitch, duration, velocity=20):
+def newNote(pitch, duration, vel=20):
     noteVal = parsePitch(pitch)
+    ticks = parseDuration(duration)
+    on = midi.NoteOnEvent(tick=ticks[0], velocity=vel, pitch=noteVal)
+    off = midi.NoteOffEvent(tick=ticks[1], pitch=noteVal)
+    return (on, off)
 
 # Format must follow -> LETTER(ACCIDENTAL)_OCTAVE
 # Returns MIDI value (int) associated with the note or -1 if a rest
 def parsePitch(pitch):
     if pitch == 'R': return -1
     note, octave = pitch.split('_')
-    if int(octave) > 8 or int(octave) < 0: raise NoteError('Octave out of range: [0, 8].')
+    if int(octave) > 8 or int(octave) < 0: raise PitchError('Octave out of range: [0, 8].')
     if len(note) is 2: note, acc = note
     else: acc = 0
     case= {
@@ -24,6 +31,46 @@ def parsePitch(pitch):
     }
     return ((int(octave) * 12) + case.get(note) + case.get(acc))
 
+# Takes in a standard note duration:
+#   whole = 1
+#   half = 2
+#   quarter = 4
+#   eighth = 8
+#   sixteenth = 16
+#   thirty-second = 32
+#   sixty-fourth = 64
+# Returns tuple (startTick, endTick) for parsing in the newNote function
+def parseDuration(duration):
+    case= {
+        1 : 64,
+        2 : 32,
+        4 : 16,
+        8 : 8,
+        16: 4,
+        32: 2,
+        64: 1
+    }
+    startTick = 0
+    endTick = case.get(duration)
+    return (startTick, endTick)
+
+if __name__ == '__main__':
+    pattern = midi.Pattern(resolution=16)
+    track = midi.Track()
+    pattern.append(track)
+
+    note = newNote('C_5', 16, 20)
+    track.append(note[0])   # midi.on event
+    track.append(note[1])   #midi.off event
+
+    eot = midi.EndOfTrackEvent(tick=1)
+    track.append(eot)
+    print(pattern)
+    midi.write_midifile("example.mid", pattern)
+
+
+################################################################################
+# Exception definitions
 ################################################################################
 class NoteError(Exception):
     # Base class for Note errors.
