86962ebffcdc174708ead857d076f9f21c2954ca
nfadili <nfadili@uw.edu>
2016-04-05 17:38:25 -0700

    Refactored tempo functions into a parser utility function and an addTempoEvent function for ease of use. Also fixed a bug in which the initial song tempo is overwritten by the last tempo event. Song() now has a globalTickCounter that acts as a master clock and a startTempo field for adding the initial tempo at the end. (This fixes the bug).

--- src/Song.py
+++ src/Song.py
@@ -3,37 +3,50 @@ from Note import *
 from constants import *
 
 class Song:
+
     # Reference to tracks are immedietly added to the midi.Pattern object.
-    def __init__(self, numTracks, tempo):
+    def __init__(self, numTracks, tempo=120):
+        self.globalTickCount = 0
+        self.startTempo = tempo
         self.pattern = midi.Pattern(resolution=MIDI_RESOLUTION)
-        self.pattern.append([midi.SetTempoEvent(tick=0, data=self.addTempoEvent(tempo)),
-                             midi.EndOfTrackEvent(tick=1, data=[])])
+        self.pattern.append([])                 # Add initial track for tempo changes
+        self.addTempoEvent(tempo)               # Add initial tempo
         self.track = []
         for i in range(numTracks):
             self.track.append(midi.Track())
             self.pattern.append(self.track[i])
 
-    # Interfaces with the Note.py file to add notes to a designated track in the song.
+    # Interfaces with the Note.py file to add a note to a designated track in the song.
     def addNoteToTrack(self, trackNum, note, octave, duration, vel=20):
+        self.globalTickCount += parseDuration(duration)[1]      # Duration of note is added to global count
         note = str(note) + '_' + str(octave)
         note = newNote(note, duration, vel)
         self.track[trackNum].append(note[0])    # midi on event
         self.track[trackNum].append(note[1])    # midi off event
 
+    # Interfaces with the Note.py file to add a rest to a designated track in the song.
     def addRestToTrack(self, trackNum, duration):
+        self.globalTickCount += parseDuration(duration)[1]      # Duration of rest is added to global count
         rest = newRest(duration)
         self.track[trackNum].append(rest[0])    # rests require only midi on event. (NOTE check this)
 
     # Appends a midi event to mark the end of a track to all tracks in the song.
     # Must be called before writing the midi data to a file.
     def markSongEnd(self):
-        eot = midi.EndOfTrackEvent(tick=0)
+        eot = midi.EndOfTrackEvent(tick=1)
+        self.addTempoEvent(self.startTempo)
+        self.pattern[0].append(eot)
         for track in self.track:
             track.append(eot)
 
+    # Adds a tempo event to the midi track at the current (relative) tick of the song.
+    # Current tick is specified with self.globalTickCount
+    def addTempoEvent(self, tempo):
+        self.pattern[0].append(midi.SetTempoEvent(tick=self.globalTickCount, data=self.parseTempoData(tempo)))
+
     # Formats a given integer tempo into -> data[hex1, hex2, hex3].
     # Must be called when midi.SetTempoEvent is called
-    def addTempoEvent(self, tempo):
+    def parseTempoData(self, tempo):
         hexString = hex(MICROSECONDS_PER_MINUTE / tempo)    # calculates microseconds per beat
         digits = []
         i = len(hexString)
@@ -51,19 +64,27 @@ if __name__ == '__main__':
     song.addNoteToTrack(0, 'A', 4, QUARTER)
     song.addNoteToTrack(0, 'C', 5, HALF)
     song.addNoteToTrack(0, 'E', 4, HALF)
+    song.addTempoEvent(120)
     song.addNoteToTrack(0, 'C', 4, HALF)
-    song.addRestToTrack(0, QUARTER)
-    song.addRestToTrack(0, QUARTER)
-    song.addRestToTrack(0, QUARTER)
+    song.addTempoEvent(80)
+    song.addNoteToTrack(0, 'A', 4, QUARTER)
+    song.addNoteToTrack(0, 'C', 5, HALF)
+    song.addNoteToTrack(0, 'E', 4, HALF)
     song.addNoteToTrack(0, 'C', 4, HALF)
-    song.addNoteToTrack(1, 'A', 4, QUARTER)
-    song.addNoteToTrack(1, 'C', 5, HALF)
-    song.addNoteToTrack(1, 'E', 4, HALF)
-    song.addNoteToTrack(1, 'C', 4, HALF)
-    song.addRestToTrack(1, QUARTER)
-    song.addRestToTrack(1, QUARTER)
-    song.addRestToTrack(1, QUARTER)
-    song.addNoteToTrack(1, 'C', 4, HALF)
+    song.addTempoEvent(100)
+    song.addNoteToTrack(0, 'A', 4, QUARTER)
+    song.addNoteToTrack(0, 'C', 5, HALF)
+    song.addNoteToTrack(0, 'E', 4, HALF)
+    song.addNoteToTrack(0, 'C', 4, HALF)
+    song.addNoteToTrack(0, 'C', 4, HALF)
+    # song.addNoteToTrack(1, 'A', 4, QUARTER)
+    # song.addNoteToTrack(1, 'C', 5, HALF)
+    # song.addNoteToTrack(1, 'E', 4, HALF)
+    # song.addNoteToTrack(1, 'C', 4, HALF)
+    # song.addRestToTrack(1, QUARTER)
+    # song.addRestToTrack(1, QUARTER)
+    # song.addRestToTrack(1, QUARTER)
+    # song.addNoteToTrack(1, 'C', 4, HALF)
 
 
     song.markSongEnd()
