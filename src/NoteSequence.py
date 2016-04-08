017f09d795a433b4413f90feead5b38a4572bfa6
nfadili <nfadili@uw.edu>
2016-04-07 18:23:32 -0700

    Added functions for parsing user input to specify the desired key. Needs a bit more testing.

--- src/NoteSequence.py
+++ src/NoteSequence.py
@@ -1,6 +1,19 @@
 import Song
 from constants import *
 
+################################################################################
+# Exception definitions
+################################################################################
+class NoteSequenceError(Exception):
+    pass
+
+class InvalidKeyError(NoteSequenceError):
+    def __init__(self, msg):
+        self.msg = msg
+
+################################################################################
+# Inner class
+################################################################################
 class Note:
     def __init__(self, letter, octave, duration, velocity=20):
          self.letter = letter
@@ -11,6 +24,9 @@ class Note:
     def __str__(self):
         return '(' + self.letter + str(self.octave) + ', ' + str(self.duration) + ', ' + str(self.velocity) + ')'
 
+################################################################################
+# NoteSequence class
+################################################################################
 # This class enforces music theory rules while generating sequences of notes for
 # insertion into a Song object. Sequences are inserted into individual tracks within
 # the Song object.
@@ -19,8 +35,10 @@ class NoteSequence:
     def __init__(self, key, voice, sequenceLength):
         self.length = sequenceLength
         self.voice = voice          # Determines starting octave
-        self.key = key
-        self.noteHistory = []       # Start with only previous note (access with [-1]), then look back farther. (Higher order markov chain).
+        self.root = ''              # Set in the parseKey function
+        self.keyDescription = ''
+        self.key = self.parseKey(key)
+        self.noteHistory = []       # Start with only previous note, then look back farther. (Higher order markov chain).
         self.sequence = self.generate()
 
     def __str__(self):
@@ -30,20 +48,39 @@ class NoteSequence:
             4 : 'ALTO',
             5 : 'SOPRANO'
         }
-        string = 'Key: ' + self.key + '\n' + case.get(self.voice) + '\n['
+        string = 'Key: ' + self.root + ' '+ self.keyDescription + '\n' + case.get(self.voice) + '\n['
         for note in self.sequence:
             string += str(note)
         return string + ']'
 
     def generate(self):
-        seq = [Note(self.key, self.voice, EIGHTH, 20)] #Tuple (root, octave, duration, velocity)
-
+        seq = [Note(self.root, self.voice, EIGHTH, 20)] #Note(root, octave, duration, velocity)
+        while len(seq) < self.length:
+            seq.append(Note(self.root, self.voice, EIGHTH, 20))
+            # newNote = self.getNextNote()
+            # seq.append(newNote)
+            # self.noteHistory.append(newNote.letter)
         return seq
 
-    def getNextNote(self):
-        case = {
+    def parseKey(self, key):
+        tokens = key.split(' ', 1)
+        self.root = tokens[0]
+        self.keyDescription = tokens[1]
+        key = self.defineKey(tokens[1:])
+        if key is not InvalidKeyError: return key
+    else: return None       # Throw error here or maybe later
 
+
+    def defineKey(self, token):
+        case = {
+            'major' : MAJOR_SCALE,
+            'natural minor' : MINOR_SCALE_NATURAL,
+            'harmonic minor' : MINOR_SCALE_HARMONIC
         }
+        return case.get(str(token[0]).lower(), InvalidKeyError('Invalid key.'))
+
+    def getNextNote(self):
+        return None
 
     def writeSequenceToTrack(self):
         # Loop through and add to a Song object with addNoteToTrack
@@ -51,9 +88,6 @@ class NoteSequence:
         pass
 
 
-
-
-
 if __name__ == '__main__':
-    seq = NoteSequence('C', SOPRANO, 20)
+    seq = NoteSequence('C# Major', SOPRANO, 20)
     print(seq)
